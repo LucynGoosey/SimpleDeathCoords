cf6106d7f85967ce749fb041d3ff3d0e402f7423
Lucyndifer <didgeridev@gmail.com>
2022-06-18 19:56:24 -0400

    Initial commit

---
+++ src/main/java/me/lucyn/simpledeathcoordinates/SimpleDeathCoordinates.java
@@ -0,0 +1,54 @@
+package me.lucyn.simpledeathcoordinates;
+
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.PlayerDeathEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.logging.Logger;
+
+import static org.bukkit.World.Environment.*;
+
+public final class SimpleDeathCoordinates extends JavaPlugin implements Listener {
+public static JavaPlugin plugin;
+    @Override //used to override the onEnable and onDisable methods from JavaPlugin
+    public void onEnable() {
+        this.saveDefaultConfig(); // creates the config file
+        getServer().getPluginManager().registerEvents(this, this); // registers the death listener
+    }
+    @Override
+    public void onDisable() {}
+
+    @EventHandler
+    public void onPlayerDeath(PlayerDeathEvent event) {
+        Player player = event.getEntity(); //gets the player that died
+       // if(!player.hasPermission("simpledeathcoordinates.use")) return; //checks if the player has permission to use the plugin
+        String message = getConfig().getString("message"); //gets the message out of the config file
+
+            message = message.replaceAll("%x%", String.valueOf(player.getLocation().getBlockX())); //replaces the placeholder with the x coordinate
+
+            message = message.replaceAll("%y%", String.valueOf(player.getLocation().getBlockY())); //replaces the placeholder with the y coordinate
+
+            message = message.replaceAll("%z%", String.valueOf(player.getLocation().getBlockZ())); //replaces the placeholder with the z coordinate
+
+            message = message.replaceAll("%world%", player.getLocation().getWorld().getName()); //replaces the placeholder with the world name
+
+        String dimension = "";
+
+        if(player.getWorld().getEnvironment() == NETHER) dimension = getConfig().getString("NETHER"); //gets the nether message from the config file
+
+        else if (player.getWorld().getEnvironment() == NORMAL) dimension = getConfig().getString("NORMAL"); //gets the normal message from the config file
+
+
+        else if (player.getWorld().getEnvironment() == THE_END) dimension = getConfig().getString("THE_END"); //gets the end message from the config file
+
+        else if(player.getWorld().getEnvironment() == CUSTOM) dimension = getConfig().getString("CUSTOM"); //gets the custom message from the config file
+
+            message = message.replaceAll("%dimension%", dimension); //replaces the placeholder with the dimension id
+
+        player.sendMessage(message); //sends the message to the player
+
+
+    }
+}
